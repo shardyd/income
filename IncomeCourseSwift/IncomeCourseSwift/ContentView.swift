3a88b389af5e712993a3c2fc323b486815124547
chard <chardsonmiranda@hotmail.com>
2026-03-17 11:45:12 -0300

    Inicio projeto swift

---
+++ IncomeCourseSwift/IncomeCourseSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeCourseSwift
+//
+//  Created by Horr on 17/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IncomeCourseSwift/IncomeCourseSwift/IncomeCourseSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeCourseSwiftApp.swift
+//  IncomeCourseSwift
+//
+//  Created by Horr on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IncomeCourseSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
